9a9fdc816d5d85ec6fb76b22808a9a238909b09d
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2022-12-22 22:20:32 +0100

    Complete Day 22

--- 22.py
+++ 22.py
@@ -1,87 +1,127 @@
-from aoc import get_input
 import re
 
-file = "22t.txt"
+grid = []
+done = False
+sequence = ""
 
-def turn(move, direction):
-    match direction:
-        case 'R':
-            match move:
-                case [1, 0]: return [0, 1]
-                case [0, 1]: return [-1, 0]
-                case [-1, 0]: return [0, -1]
-                case [0, -1]: return [1, 0]
-                case _: print("Incorrect move statement! Move: " + str(move)); assert False
-        case 'L':
-            match move:
-                case [1, 0]: return [0, -1]
-                case [0, 1]: return [1, 0]
-                case [-1, 0]: return [0, 1]
-                case [0, -1]: return [1, 0]
-                case _: print("Incorrect move statement! Move: " + str(move)); assert False
-        case _: print("Incorrect turn direction! Direction input: " + str(direction)); assert False
+for line in open("22t.txt"):
+    line = line[:-1]
+    if line == "":
+        done = True
+    if done:
+        sequence = line
+    else:
+        grid.append(line)
 
-map = []
-task_list_str = ""
-task_list_bool = False
-player_pos = [0, 0]
+width = max(map(len, grid))
+grid = [line + " " * (width - len(line)) for line in grid]
+
+position = [0, 0]
+next_pos = [0, 0]
 move = [1, 0]
 
-for i, row in enumerate(open(file)):
-    if row == "\n":
-        task_list_bool = True
-        continue
-    if task_list_bool:
-        task_list_str = row
-        break    
-    row_as_list = []
-    for char in row:
-        match char:
-            case ' ': row_as_list.append("a")
-            case '#': row_as_list.append("w")
-            case '.': row_as_list.append(".")
-            case '\n': continue
-            case _: print("Input error! Invalid character: " + char); assert False
-    map.append(row_as_list)
+while grid[position[1]][position[0]] != ".":
+    position[0] += 1
 
-temp_tasks = re.split('(\d+)', task_list_str)
-
-while temp_tasks.count('') > 0:
-    temp_tasks.remove('')
-    
-tasks = []
-    
-for task in temp_tasks:
-    try:
-        tasks.append(int(task))
-    except:
-        for char in task:
-            tasks.append(char)
-
-for i, tile in enumerate(map[0]):
-    if tile == '.':
-        player_pos[0] = i
-        break
-#print(player_pos)        
-        
-for row in map:
-    for tile in row:
-        print(tile, end='')
-    print()
-
-for task in tasks:
-    print(task)
-    if task == 'L' or task == 'R':
-        move = turn(move, task)
-    else:
-        for _ in range(task):
-            prospective_move = [player_pos[0] + move[0], player_pos[1] + move[1]]
-            
-            if map[prospective_move[1]][prospective_move[0]] == "w":
+for x, y in re.findall(r"(\d+)([RL]?)", sequence):
+    x = int(x)
+    for _ in range(x):
+        next_pos[0] = position[0]
+        next_pos[1] = position[1]
+        while True:
+            next_pos[1] = (next_pos[1] + move[1]) % len(grid)
+            next_pos[0] = (next_pos[0] + move[0]) % len(grid[0])
+            if grid[next_pos[1]][next_pos[0]] != " ":
                 break
-            else:
-                player_pos = prospective_move
-        print(player_pos)
-                
-print(player_pos)
+        if grid[next_pos[1]][next_pos[0]] == "#":
+            break
+        position[0] = next_pos[0]
+        position[1] = next_pos[1]
+    if y == "R":
+        move[1], move[0] = move[0], -move[1]
+    elif y == "L":
+        move[1], move[0] = -move[0], move[1]
         
+mod = 0
+match move:
+    case [1, 0]: mod = 0
+    case [0, 1]: mod = 1
+    case [-1, 0]: mod = 2
+    case [0, -1]: mod = 3
+
+print("Part 1: " + str(1000 * (position[1] + 1) + 4 * (position[0] + 1) + mod))
+
+position = [0, 0]
+next_pos = [0, 0]
+move = [1, 0]
+
+while grid[position[1]][position[0]] != ".":
+    position[0] += 1
+
+for x, y in re.findall(r"(\d+)([RL]?)", sequence):
+    x = int(x)
+    for _ in range(x):
+        pre_move = move[:]
+        
+        next_pos[0] = position[0] + move[0]
+        next_pos[1] = position[1] + move[1]
+        
+        if next_pos[1] < 0 and 50 <= next_pos[0] < 100 and move[1] == -1:
+            move[1], move[0] = 0, 1
+            next_pos[1], next_pos[0] = next_pos[0] + 100, 0
+        elif next_pos[0] < 0 and 150 <= next_pos[1] < 200 and move[0] == -1:
+            move[1], move[0] = 1, 0
+            next_pos[1], next_pos[0] = 0, next_pos[1] - 100
+        elif next_pos[1] < 0 and 100 <= next_pos[0] < 150 and move[1] == -1:
+            next_pos[1], next_pos[0] = 199, next_pos[0] - 100
+        elif next_pos[1] >= 200 and 0 <= next_pos[0] < 50 and move[1] == 1:
+            next_pos[1], next_pos[0] = 0, next_pos[0] + 100
+        elif next_pos[0] >= 150 and 0 <= next_pos[1] < 50 and move[0] == 1:
+            move[0] = -1
+            next_pos[1], next_pos[0] = 149 - next_pos[1], 99
+        elif next_pos[0] == 100 and 100 <= next_pos[1] < 150 and move[0] == 1:
+            move[0] = -1
+            next_pos[1], next_pos[0] = 149 - next_pos[1], 149
+        elif next_pos[1] == 50 and 100 <= next_pos[0] < 150 and move[1] == 1:
+            move[1], move[0] = 0, -1
+            next_pos[1], next_pos[0] = next_pos[0] - 50, 99
+        elif next_pos[0] == 100 and 50 <= next_pos[1] < 100 and move[0] == 1:
+            move[1], move[0] = -1, 0
+            next_pos[1], next_pos[0] = 49, next_pos[1] + 50
+        elif next_pos[1] == 150 and 50 <= next_pos[0] < 100 and move[1] == 1:
+            move[1], move[0] = 0, -1
+            next_pos[1], next_pos[0] = next_pos[0] + 100, 49
+        elif next_pos[0] == 50 and 150 <= next_pos[1] < 200 and move[0] == 1:
+            move[1], move[0] = -1, 0
+            next_pos[1], next_pos[0] = 149, next_pos[1] - 100
+        elif next_pos[1] == 99 and 0 <= next_pos[0] < 50 and move[1] == -1:
+            move[1], move[0] = 0, 1
+            next_pos[1], next_pos[0] = next_pos[0] + 50, 50
+        elif next_pos[0] == 49 and 50 <= next_pos[1] < 100 and move[0] == -1:
+            move[1], move[0] = 1, 0
+            next_pos[1], next_pos[0] = 100, next_pos[1] - 50
+        elif next_pos[0] == 49 and 0 <= next_pos[1] < 50 and move[0] == -1:
+            move[0] = 1
+            next_pos[1], next_pos[0] = 149 - next_pos[1], 0
+        elif next_pos[0] < 0 and 100 <= next_pos[1] < 150 and move[0] == -1:
+            move[0] = 1
+            next_pos[1], next_pos[0] = 149 - next_pos[1], 50
+        
+        if grid[next_pos[1]][next_pos[0]] == "#":
+            move = pre_move[:]
+            break
+        position[0] = next_pos[0]
+        position[1] = next_pos[1]
+    if y == "R":
+        move[1], move[0] = move[0], -move[1]
+    elif y == "L":
+        move[1], move[0] = -move[0], move[1]
+
+mod = 0
+match move:
+    case [1, 0]: mod = 0
+    case [0, 1]: mod = 1
+    case [-1, 0]: mod = 2
+    case [0, -1]: mod = 3
+    
+print("Part 2: " + str(1000 * (position[1] + 1) + 4 * (position[0] + 1) + mod))
